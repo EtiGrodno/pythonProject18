3c533db6ff538ae68d85888d812c78797421cbfc
EtiGrodno <eti18@bk.ru>
2023-05-26 12:48:43 +0300

    домашка №18 от 25.05.2023 исправленна

--- Charniauski_Lesson_18.py
+++ Charniauski_Lesson_18.py
@@ -5,33 +5,30 @@ class BankAccount:
     __account_number = None
     __balance = 0
 # Инициализатор __init__ используется для инициализации номера счета и начального баланса.
-    def __init__(self, namber_acc = __account_number, elementary_bal = __balance):
-        self.namber_acc = namber_acc
-        self.elementary_bal = elementary_bal
+    def __init__(self,__account_number, __balance):
+        self.__account_number = __account_number
+        self.__balance = __balance
+
 # Методы get_account_number и get_balance предоставляют
 # доступ к приватным свойствам __account_number и __balance соответственно.
     def get_account_number(self):
-        print(f'Номер счёта: {self.namber_acc}')
+        print(f'Номер счёта: {self.__account_number}')
     def get_balance(self):
-        print(f'Баланс: {self.elementary_bal}')
+        print(f'Баланс: {self.__balance}')
 # Методы deposit и withdraw позволяют пополнять и снимать деньги со счета,
 # при этом проверяя валидность операции (достаточно ли средств, корректно ли введена сумма для снятия).
     def deposit(self, popolneniye):
-        self.elementary_bal += popolneniye
-        print(f'Получено {popolneniye} рублей. Новый счет: {self.elementary_bal}')
+        self.__balance += popolneniye
+        print(f'Получено {popolneniye} рублей. Новый счет: {self.__balance}')
     def withdraw(self, snyatiye):
-        if self.elementary_bal >= snyatiye:
-            self.elementary_bal -= snyatiye
-            print(f'Снято {snyatiye} рублей. Новый счет: {self.elementary_bal}')
+        if self.__balance >= snyatiye:
+            self.__balance -= snyatiye
+            print(f'Снято {snyatiye} рублей. Новый счет: {self.__balance}')
         else:
-            print(f'Недостаточно средств. Свертесь с балансом счёта: {self.elementary_bal}')
+            print(f'Недостаточно средств. Свертесь с балансом счёта: {self.__balance}')
 
 #В основной части кода мы создаем экземпляр класса BankAccount
 # с номером счета "123456789" и начальным балансом 1000.
-
-my_BankAccount_privat = BankAccount()
-my_BankAccount_privat.get_account_number()
-my_BankAccount_privat.get_balance()
 
 my_BankAccount = BankAccount("123456789",1000)
 my_BankAccount.get_account_number()
